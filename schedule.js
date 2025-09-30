ada9175b0e1fb496a70e7ea70232a104ce6e9858
ibrahim1-crypto <ibrahim1@aiesec.net>
2025-09-30 02:37:37 +0200

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,7 +1,7 @@
-const { WebClient } = require('@slack/web-api');
+import { WebClient } from '@slack/web-api';
 
 const web = new WebClient(process.env.SLACK_BOT_TOKEN);
-const channelId = 'C097RTHM23X'; // e.g., C0123456789
+const channelId = 'C097RTHM23X';
 
 const messageText =
   ':wave: Hi everyone! Please send your FA updates for the week by replying in this channel.\n' +
@@ -15,27 +15,23 @@ const messageText =
   '---\n' +
   'Thank you!';
 
-// Compute next Monday 09:00 at GMT+2 (fixed offset +02:00)
 function nextMondayAt0900_GMTplus2_toUnix() {
   const now = new Date();
-  const day = now.getUTCDay(); // 0=Sun..6=Sat, UTC-based
+  const day = now.getUTCDay();
   const daysUntilMon = (1 - day + 7) % 7 || 7;
-
-  // Build a UTC date for the next Monday at 07:00 UTC, which equals 09:00 at GMT+2
-  // Note: GMT+2 is UTC+2, so 09:00 local = 07:00 UTC
-  const targetUtcHour = 7;
+  // 09:00 at GMT+2 = 07:00 UTC
   const d = new Date(Date.UTC(
     now.getUTCFullYear(),
     now.getUTCMonth(),
     now.getUTCDate() + daysUntilMon,
-    targetUtcHour, 0, 0, 0
+    7, 0, 0, 0
   ));
   return Math.floor(d.getTime() / 1000);
 }
 
-(async () => {
-  const post_at = nextMondayAt0900_GMTplus2_toUnix();
-  const res = await web.chat.scheduleMessage({ channel: channelId, text: messageText, post_at });
-  console.log('Scheduled:', res.scheduled_message_id);
-})();
+const post_at = nextMondayAt0900_GMTplus2_toUnix();
+
+const res = await web.chat.scheduleMessage({ channel: channelId, text: messageText, post_at });
+console.log('Scheduled:', res.scheduled_message_id);
+
 
